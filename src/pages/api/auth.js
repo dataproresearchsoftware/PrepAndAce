e86921146c488eed569f0ddd1fe86ed412bc70df
Waqar Abid <abid.waqar@gmail.com>
2024-04-12 16:36:07 +0500

    Unauthorized error find

--- src/pages/api/auth.js
+++ src/pages/api/auth.js
@@ -181,10 +181,10 @@ export default async function handler(req, res) {
                 }
               )
             } else {
-              res.status(401).json({ error: 'Unauthorized!' })
+              res.status(401).json({ error: 'Unauthorized!', errorDetail: 'false' })
             }
           } catch (error) {
-            res.status(401).json({ error: 'Unauthorized!' })
+            res.status(401).json({ error: `Unauthorized!`, errorDetail: error })
           }
         }
         break
